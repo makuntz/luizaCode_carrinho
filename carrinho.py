94adb9471d5688b80ea3cbefacedf5c6d2f702c9
makuntz <maira.kuntz@animati.com.br>
2022-09-01 19:43:44 -0300

    primeiros comandos do carrinho

---
+++ carrinho.py
@@ -0,0 +1,24 @@
+
+cart = []
+#for i in range(3)
+
+id_user = input("Insira o id do usuário: ")
+id_product = input("Insira o id do produto: ")
+price_product = float(input("Insira o preço do produto: "))
+quantity_product = int(input("Insira a quantidade de produtos: "))
+
+item = [id_user, id_product, price_product, quantity_product]
+
+def add_item_cart(item):
+    pass
+
+def get_all_items_cart():
+    #return todos os itens do carrinho
+    pass
+    
+def get_item_cart_by_id(id_product):
+    pass
+
+def remove_item_id(id_product):
+    #remover o item do carrinho que tem esse produto
+    pass
